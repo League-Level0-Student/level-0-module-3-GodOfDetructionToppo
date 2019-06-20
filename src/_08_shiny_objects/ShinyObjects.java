b72aae107a8277a761e75e2ca7d2dd255679800d
ilabstaff <ilabstaff@172.16.116.77>
2019-06-19 17:53:34 -0700

    finish fizzbuzz and shiny object and started on are you happy

--- src/_08_shiny_objects/ShinyObjects.java
+++ src/_08_shiny_objects/ShinyObjects.java
@@ -8,14 +8,22 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
+		String num= JOptionPane.showInputDialog("How many shiny object do you want");
 
+		int num2=Integer.parseInt(num);
+
+	
 		// 3. Play the sound that many times
-
+		for(int i=0;i< num2; i++) {
+			playMisterZee();
+	
+}
 		// 1. Call the method below
 
 	}
