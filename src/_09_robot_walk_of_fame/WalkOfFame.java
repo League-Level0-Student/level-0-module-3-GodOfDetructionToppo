c02c4e1c058cf693c8029eb1f128c1801640c45e
kaylarwright <kayla.wright@gompersstudent.org>
2019-09-18 17:52:56 -0700

    work_on_WalkOfFame

--- src/_09_robot_walk_of_fame/WalkOfFame.java
+++ src/_09_robot_walk_of_fame/WalkOfFame.java
@@ -4,6 +4,8 @@
 
 package _09_robot_walk_of_fame;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
@@ -12,7 +14,10 @@ public class WalkOfFame {
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
 		rob.setX(1);
-
+		String shape= JOptionPane.showInputDialog(null,"which shape do you want");
+		rob.penDown();
+		rob.setSpeed(55);
+		if(shape.e
 		// 2. Make the robot draw a star shape. Hint: 144.
 
 		// 3. Set the size of the star to 30.
